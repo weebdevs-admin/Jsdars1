03b7cbeff4ee9c230e5fdabbc7d77c142504bf23
weebhacker <weebhacker1@gmail.com>
2022-12-10 08:18:29 +0300

    add

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
-alert('lacatsiyaga ruxsat berasanmi?');
-alert("fayllaringa kirishimga ruxsat berasanmi?");
+alert('Xush Kelibsiz!')
+alert('lacatsiyaga ruxsat berasizmi?');
+alert("fayllarga kirishimga ruxsat berasizmi?");
 const myname = prompt('ismingizni kiriting.. ');
 const phone = prompt("telefon raqamingizni kiriting.. ");
 const year = prompt("Tug'ilgan yilingizni kiriting.. ");
